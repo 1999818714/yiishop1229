de2119f5eb7b94df9d563d76ec01060be1baf8f5
flyok666 <flyok666@qq.com>
2017-04-22 11:55:24 +0800

    微信开发

---
+++ frontend/controllers/ApiController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/18
+ * Time: 10:15
+ */
+
+namespace frontend\controllers;
+
+
+use yii\web\Controller;
+
+class ApiController extends Controller
+{
+    public function actionLogin()
+    {
+        echo 'login';
+    }
+
+}
